a4b6813ffb1447f788b7f2ca7e457fdcf3b9d3eb
Michał "phoe" Herda <phoe@disroot.org>
2020-02-29 11:54:25 +0100

    Fix NOTF being used before it is declared

--- phoe-toolbox.lisp
+++ phoe-toolbox.lisp
@@ -156,6 +156,71 @@ a condition of type CONDITION-TYPE was signaled exactly N times."
            (unless (= 1 ,n)
              (error ,error-msg-3 ',condition-type ,count)))))))
 
+(define-modify-macro notf () not
+  "Sets the value of PLACE to its logical negation and returns the new value.")
+
+;;; The following implementations of MOD-INCF and MOD-DECF have been adapted
+;;; from SICL by Robert Strandh and are subject to the following license:
+
+;;; Copyright (c) 2010 - 2016
+;;;
+;;;     Robert Strandh (robert.strandh@gmail.com)
+;;;
+;;; All rights reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or
+;;; without modification, are permitted provided that the following
+;;; conditions are met:
+;;;
+;;; 1. Redistributions of source code must retain the above copyright
+;;;    notice, this list of conditions and the following disclaimer.
+;;; 2. Redistributions in binary form must reproduce the above
+;;;    copyright notice, this list of conditions and the following
+;;;    disclaimer in the documentation and/or other materials
+;;;    provided with the distribution.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+;;; CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+;;; INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+;;; MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+;;; BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+;;; ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+;;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+;;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+
+(defmacro mod-incf (&environment env place divisor &optional (delta-form 1))
+  "Like INCF, except the final value set in PLACE is always a modulus modulo
+DIVISOR."
+  (multiple-value-bind (vars vals store-vars writer-form reader-form)
+      (get-setf-expansion place env)
+    (if (null vars)
+        `(let ((,(first store-vars)
+                 (mod (+ ,reader-form ,delta-form) ,divisor)))
+           ,writer-form)
+        `(let* ((,(first vars) ,(first vals))
+                (,(first store-vars)
+                  (mod (+ ,reader-form ,delta-form) ,divisor)))
+           ,writer-form))))
+
+(defmacro mod-decf (&environment env place divisor &optional (delta-form 1))
+  "Like DECF, except the final value set in PLACE is always a modulus modulo
+DIVISOR."
+  (multiple-value-bind (vars vals store-vars writer-form reader-form)
+      (get-setf-expansion place env)
+    (if (null vars)
+        `(let ((,(first store-vars)
+                 (mod (- ,reader-form ,delta-form) ,divisor)))
+           ,writer-form)
+        `(let* ((,(first vars) ,(first vals))
+                (,(first store-vars)
+                  (mod (- ,reader-form ,delta-form) ,divisor)))
+           ,writer-form))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Functions
 
@@ -500,68 +565,3 @@ value from the returned function."
     (lambda (&rest rest)
       (declare (ignore rest))
       (if (notf x) value-when-true value-when-false))))
-
-;; The following implementations of MOD-INCF and MOD-DECF have been adapted from
-;; SICL by Robert Strandh and are subject to the following license:
-
-;;;; Copyright (c) 2010 - 2016
-;;;;
-;;;;     Robert Strandh (robert.strandh@gmail.com)
-;;;;
-;;;; All rights reserved.
-;;;;
-;;;; Redistribution and use in source and binary forms, with or
-;;;; without modification, are permitted provided that the following
-;;;; conditions are met:
-;;;;
-;;;; 1. Redistributions of source code must retain the above copyright
-;;;;    notice, this list of conditions and the following disclaimer.
-;;;; 2. Redistributions in binary form must reproduce the above
-;;;;    copyright notice, this list of conditions and the following
-;;;;    disclaimer in the documentation and/or other materials
-;;;;    provided with the distribution.
-;;;;
-;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-;;;; CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-;;;; INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-;;;; MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-;;;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-;;;; BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-;;;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-;;;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-;;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-;;;; ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-;;;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-;;;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-;;;; POSSIBILITY OF SUCH DAMAGE.
-
-(defmacro mod-incf (&environment env place divisor &optional (delta-form 1))
-  "Like INCF, except the final value set in PLACE is always a modulus modulo
-DIVISOR."
-  (multiple-value-bind (vars vals store-vars writer-form reader-form)
-      (get-setf-expansion place env)
-    (if (null vars)
-        `(let ((,(first store-vars)
-                 (mod (+ ,reader-form ,delta-form) ,divisor)))
-           ,writer-form)
-        `(let* ((,(first vars) ,(first vals))
-                (,(first store-vars)
-                  (mod (+ ,reader-form ,delta-form) ,divisor)))
-           ,writer-form))))
-
-(defmacro mod-decf (&environment env place divisor &optional (delta-form 1))
-  "Like DECF, except the final value set in PLACE is always a modulus modulo
-DIVISOR."
-  (multiple-value-bind (vars vals store-vars writer-form reader-form)
-      (get-setf-expansion place env)
-    (if (null vars)
-        `(let ((,(first store-vars)
-                 (mod (- ,reader-form ,delta-form) ,divisor)))
-           ,writer-form)
-        `(let* ((,(first vars) ,(first vals))
-                (,(first store-vars)
-                  (mod (- ,reader-form ,delta-form) ,divisor)))
-           ,writer-form))))
-
-(define-modify-macro notf () not
-  "Sets the value of PLACE to its logical negation and returns the new value.")
